8a5ae9a0e3d9dcd1452e816d821eebed5c358b47
PotatoDotCat <162398229+PotatoDotCat@users.noreply.github.com>
2024-05-08 16:02:58 +1200

    Update samestartandend.py

--- samestartandend.py
+++ samestartandend.py
@@ -1,14 +1,9 @@
 stringsList = ["abc", "123", "2332", "aBBA", "heelloo", "1212", "DcEfD"]
-
-# Initialize a counter for matching strings
 matching_count = 0
 
-# Check each string in the list
 for s in stringsList:
-    # Convert the string to lowercase for case-insensitive comparison
     s_lower = s.lower()
     
-    # Check if the first and last characters are the same
     if s_lower[0] == s_lower[-1]:
         matching_count += 1
 
